9a24ba22aef9440ace11fad8235d22659ca31fa8
Cristina <Cristina.Cabral@triosstudent.com>
2025-03-07 18:35:54 -0500

    Initial Commit

---
+++ HockeyPro/HockeyProApp.swift
@@ -0,0 +1,17 @@
+//
+//  HockeyProApp.swift
+//  HockeyPro
+//
+//  Created by Cristina Cabral on 2025-03-07.
+//
+
+import SwiftUI
+
+@main
+struct HockeyProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
